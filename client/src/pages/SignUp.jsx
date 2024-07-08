3147d4a69d6e4cb938167eefe97271d62611a9da
Nikhil Mujalde <nikhilmujalde@gmail.com>
2024-07-08 16:31:36 +0530

    proxy server added

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -21,7 +21,7 @@ const SignUp = () => {
     e.preventDefault();
     try {
       setloading(true)
-      const res = await fetch('http://localhost:3000/api/auth/signup', {
+      const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
