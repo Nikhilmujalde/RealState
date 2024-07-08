11823505ac2abffda178d8bc0b9866e8710670b1
Nikhil Mujalde <nikhilmujalde@gmail.com>
2024-07-08 10:19:12 +0530

    added .env

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 // const express = require('express')
 import express from "express"
+import mongoose from "mongoose"
+// import { configDotenv } from "dotenv"
+// configDotenv.config
+import 'dotenv/config'
 const app = express()
 const port = 3000
 
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('Connected to mongo')
+})
+.catch((err)=>{
+    console.log(err);
+})
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
