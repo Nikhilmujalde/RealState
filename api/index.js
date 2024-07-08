0e4cad0979a5a2414caa3295e92d37fa91ad3f1e
Nikhil Mujalde <nikhilmujalde@gmail.com>
2024-07-08 11:02:37 +0530

    create api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 // const express = require('express')
 import express from "express"
 import mongoose from "mongoose"
-// import { configDotenv } from "dotenv"
-// configDotenv.config
+import userRouter from "./routes/user.router.js"
 import 'dotenv/config'
 const app = express()
 const port = 3000
@@ -21,3 +20,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.use("/api/user",userRouter)
