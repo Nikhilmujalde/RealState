bce578b489316f9d6315c3254cc61b3d7ab3ad9d
Nikhil Mujalde <nikhilmujalde@gmail.com>
2024-07-10 15:36:19 +0530

    create update api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,9 +1,49 @@
 import Listing from "../models/listing.model.js"
-
+import { errorHandler } from "../utils/error.js"
+import mongoose from "mongoose"
 export const createListing=async(req,res,next)=>{
     try {
         const listing = await Listing.create(req.body)
         return res.status(201).json(listing)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deletListing=async(req,res,next)=>{
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return next(errorHandler(404, 'Listing not found'))
+    }
+    const listing = await Listing.findById(req.params.id);
+    if(!listing){
+        return next(errorHandler(404,'Listing not found'))
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401,'You can only delete your own listing'))
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id)
+        return res.status(200).json('Listing has been deleted')
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const editListing=async(req,res,next)=>{
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return next(errorHandler(404, 'Listing not found'))
+    }
+    const listing = await Listing.findById(req.params.id)
+    if(!listing){
+        return next(errorHandler(404,'Listing not found'))
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401,'You can only update your own listing'))
+    }
+    try {
+        const editListing = await Listing.findByIdAndUpdate(req.params.id,req.body,{new:true})
+        return res.status(200).json(editListing)
     } catch (error) {
         next(error)
     }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import { createListing, deletListing, editListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifiedUser.js";
 
 const router = express.Router()
 
 
 router.post('/create',verifyToken,createListing)
+router.delete('/delete/:id',verifyToken,deletListing)
+router.post('/edit/:id',verifyToken,editListing)
 
 export default router;
