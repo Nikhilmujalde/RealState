175854bdb47953deaa91940a8d1960055b45d0a4
Nikhil Mujalde <nikhilmujalde@gmail.com>
2024-07-11 16:49:18 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Real State</h1>
+      <h2 className='text-xl font-semibold text-slate-600 mb-2'>Discover Your Perfect Home with Ease</h2>
+      <p className='mb-4 text-slate-700'>Welcome to Real Estate, the ultimate destination for finding your next dream home. Our platform offers an extensive range of properties, ensuring that you find the perfect place that fits your lifestyle and budget. Whether you're looking to buy, rent, or take advantage of special offers, Real Estate provides a seamless and user-friendly experience to make your property search effortless and enjoyable.</p>
+      <h2 className='text-xl font-semibold text-slate-600 mb-2'>Unmatched Variety and Detailed Listings
+
+      </h2>
+      <p className='mb-4 text-slate-700'>At Real Estate, we pride ourselves on offering a diverse selection of listings that cater to every preference and need. From cozy apartments to spacious family homes, our detailed listings include high-quality images, comprehensive descriptions, and essential amenities to help you make informed decisions. Our advanced search filters and intuitive interface allow you to customize your search, ensuring you find properties that match your exact criteria.
+
+      </p>
+      <h2 className='text-xl font-semibold text-slate-600 mb-2'>Your Trusted Partner in Property Search
+
+      </h2>
+      <p className='mb-4 text-slate-700'>Real Estate is committed to making your property search as smooth and successful as possible. Our dedicated team continuously updates our listings and works closely with property owners and agents to provide you with the latest and most accurate information. We strive to be your trusted partner in the real estate journey, offering support and guidance every step of the way. With Real Estate, finding your perfect home has never been easier or more reliable.
+
+      </p>
     </div>
   )
 }
