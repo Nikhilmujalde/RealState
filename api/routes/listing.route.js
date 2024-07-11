792aec5c48d6b839d74e24d71973a7bcfb35396a
Nikhil Mujalde <nikhilmujalde@gmail.com>
2024-07-11 08:38:15 +0530

    create search api route and update header search

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, deletListing, editListing, getListing } from "../controllers/listing.controller.js";
+import { createListing, deletListing, editListing, getListing, getListings } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifiedUser.js";
 
 const router = express.Router()
@@ -10,5 +10,6 @@ router.delete('/delete/:id',verifyToken,deletListing)
 router.put('/edit/:id',verifyToken,editListing)
 // we don't need to verify the user here because everybody will be able to see the listing
 router.get('/getListing/:id',getListing)
+router.get('/get',getListings)
 
 export default router;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link ,useNavigate} from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
 const Header = () => {
     // to get the current user
     const {currentUser} = useSelector((state) => state.user)
+    const [searchTerm, setsearchTerm] = useState("")
+    const navigate = useNavigate()
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search)
+      const searchTermFromUrl = urlParams.get('searchTerm')
+      if(searchTermFromUrl){
+        setsearchTerm(searchTermFromUrl)
+      }
+    }, [location.search])
+    
+    const handleSubmit=async(e)=>{
+        e.preventDefault()
+        // jo bhi url hai usko ham fetch karenge or searchTerm mai dal denge
+        const urlParams = new URLSearchParams(window.location.search)
+        urlParams.set('searchTerm',searchTerm)
+        const searchQuery = urlParams.toString()
+        navigate(`/search?${searchQuery}`)
+
+    }
     return (
         <header className='bg-gray-300 shadow-md'>
             <div className="flex justify-between items-center mx-auto max-w-6xl p-3">
@@ -14,9 +34,12 @@ const Header = () => {
                     <span className='text-gray-900'>State</span>
                 </h1>
                 </Link>
-                <form action="" className='bg-gray-100 p-3 justify-between rounded-xl flex items-center w-24 sm:w-64'>
-                    <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none' />
+                <form onSubmit={handleSubmit} action="" className='bg-gray-100 p-3 justify-between rounded-xl flex items-center w-24 sm:w-64'>
+                    <input type="text" value={searchTerm} onChange={(e)=>setsearchTerm(e.target.value)} placeholder='Search...' className='bg-transparent focus:outline-none' />
+                    <button>
                     <FaSearch className='text-gray-600'/>
+                        
+                    </button>
                 </form>
                 <ul className='flex gap-4'>
                     <Link to={'/home'}>
